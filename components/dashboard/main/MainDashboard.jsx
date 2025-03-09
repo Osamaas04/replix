b013bcc403f0b42149900702338e2bdf5687a41b
Osamaas04 <alasmarosama7@gmail.com>
2025-03-09 04:13:56 +0300

    fix searchParams

--- components/dashboard/main/MainDashboard.jsx
+++ components/dashboard/main/MainDashboard.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { Suspense } from "react";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
@@ -9,11 +9,11 @@ import SocialCards from "@/components/dashboard/sub/SocialCard";
 import Automations from "../sub/Automations";
 import Logo from "@/public/assets/chatlogo-dark.webp";
 
-export default function MainDashboard() {
+function DashboardContent() {
   const router = useRouter();
   const pathname = usePathname();
   const searchParams = useSearchParams();
-  
+
   const menuItem = searchParams.get("menu") || "Home";
 
   const handleMenuChange = (menu) => {
@@ -25,7 +25,7 @@ export default function MainDashboard() {
     <div className="bg-secondary h-screen">
       <div className="flex">
         <aside className="bg-primary/5 flex flex-col justify-between w-[16rem] h-screen border-r border-primary/10 px-4 py-4">
-          <div className="grid items-baseline ">
+          <div className="grid items-baseline">
             <div className="flex items-center justify-between">
               <Link href="/dashboard" className="flex space-x-3 items-center">
                 <Image src={Logo} alt="Replix logo" width={22} height={22} />
@@ -50,7 +50,9 @@ export default function MainDashboard() {
                   }`}
                   onClick={() => handleMenuChange(name)}
                 >
-                  <li className="flex gap-4 items-center">{icon} <span>{name}</span></li>
+                  <li className="flex gap-4 items-center">
+                    {icon} <span>{name}</span>
+                  </li>
                 </button>
               ))}
             </ol>
@@ -61,5 +63,13 @@ export default function MainDashboard() {
         {menuItem === "Automations" && <Automations />}
       </div>
     </div>
+  );
+}
+
+export default function MainDashboard() {
+  return (
+    <Suspense fallback={<div>Loading Dashboard...</div>}>
+      <DashboardContent />
+    </Suspense>
   );
 }
