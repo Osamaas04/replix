efd3852bee916d9215fd5faa742a92a68e66c5ba
Osamaas04 <alasmarosama7@gmail.com>
2025-03-17 21:19:44 +0300

    front polish

--- components/sections/pricing_section/sub/Headline.jsx
+++ components/sections/pricing_section/sub/Headline.jsx
@@ -1,6 +1,6 @@
 export default function Headline() {
   return (
-    <div className="font-raleway grid justify-center gap-6 sm:gap-10">
+    <div className="font-raleway grid justify-center items-center gap-6 sm:gap-10">
       <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold text-secondary text-center ">
       Get the most out of Replix
       </h1>
