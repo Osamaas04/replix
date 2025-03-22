05f2b313a8500a062624bcb9ba00a3da12aa54fd
Osamaas04 <alasmarosama7@gmail.com>
2025-03-22 23:05:59 +0300

    front polish 37

--- components/dashboard/sub/integrations/sub/XCard.jsx
+++ components/dashboard/sub/integrations/sub/XCard.jsx
@@ -2,7 +2,7 @@ import { Switch } from "../ui/switch";
 
 export default function XCard() {
   return (
-    <div className="bg-secondary rounded-md p-8 grid gap-8  lg:w-[19rem] xl:w-[25rem]">
+    <div className="bg-secondary/90 rounded-md p-8 grid gap-8  lg:w-[19rem] xl:w-[25rem]">
       <div className="grid gap-2">
         <div className="flex items-center gap-4">
           <svg
