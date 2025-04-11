e2d6bd63e3d4f2ee94cccb1c95688161fe2b0c06
Osamaas04 <alasmarosama7@gmail.com>
2025-04-11 20:01:24 +0300

    refine 27

--- app/api/setAuthToken/route.js
+++ app/api/setAuthToken/route.js
@@ -8,14 +8,16 @@ export const POST = async (request) => {
       return NextResponse.json({ error: "Missing authorization token" }, { status: 400 });
     }
 
+    // Create the response and set the cookie with the necessary attributes
     const response = NextResponse.json(
       { message: "User token has been set successfully" },
       { status: 200 }
     );
 
+    // Set the cookie with domain, SameSite, Secure, and HttpOnly attributes
     response.headers.set(
       "Set-Cookie",
-      `token=${token}; HttpOnly; Secure; SameSite=None; Path=/`
+      `token=${token}; HttpOnly; Secure; SameSite=None; Path=/; Domain=.replix.space;`  // Max-Age = 1 hour
     );
 
     return response;
