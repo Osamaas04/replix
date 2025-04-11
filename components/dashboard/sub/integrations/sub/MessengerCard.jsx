19ada5ccb9f7c22c6483a5135dd03915fe96f5a6
Osamaas04 <alasmarosama7@gmail.com>
2025-04-11 18:38:07 +0300

    refine 26

--- components/dashboard/sub/integrations/sub/MessengerCard.jsx
+++ components/dashboard/sub/integrations/sub/MessengerCard.jsx
@@ -125,7 +125,7 @@ export default function MessengerCard() {
     const handleOAuthCallback = async () => {
       if (code) {
         try {
-          const response = await fetch("http://replix.runasp.net/social/connectFacebook", {
+          const response = await fetch(`${API_GATEWAY}/connectFacebook`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             credentials: "include",
