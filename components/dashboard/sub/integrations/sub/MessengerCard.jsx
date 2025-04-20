351934c023a2bbd5681b508cb3462a0bd54c84d2
Osamaas04 <alasmarosama7@gmail.com>
2025-04-20 20:26:54 +0300

    refine 61

--- components/dashboard/sub/integrations/sub/MessengerCard.jsx
+++ components/dashboard/sub/integrations/sub/MessengerCard.jsx
@@ -129,7 +129,6 @@ export default function MessengerCard() {
           const response = await fetch("https://gw.replix.space/hello/connectFacebook", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            credentials: "include",
             body: JSON.stringify({ code }),
           });
 
