f8f5152f9bd3ca8f23ded90b33c3937d459c0fa1
Osamaas04 <alasmarosama7@gmail.com>
2025-04-23 14:53:49 +0300

    refine 64

---
+++ components/dashboard/sub/train-ai/sub/UploadFineTune.jsx
@@ -0,0 +1,73 @@
+import { Upload, FileJson } from "lucide-react";
+import { useRef } from "react";
+import { toast } from "sonner";
+
+const API_GATEWAY = "https://gw.replix.space/files"
+
+export default function UploadFineTune() {
+  const fileInputRef = useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  async function handleFileChange(event) {
+    try {
+      const file = event.target.files[0];
+      if (!file) return;
+
+      const formData = new FormData();
+      formData.append("dataset", file);
+      formData.append("purpose", "fine-tune");
+
+      const response = await fetch(`${API_GATEWAY}`, {
+        method: "POST",
+        credentials: "include",
+        body: formData,
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to upload the file - please try again");
+      }
+
+      toast.success("File has been uploaded successfully");
+    } catch (error) {
+      toast.error("Failed to upload the file - please try again");
+    }
+  }
+
+  return (
+    <div className="w-full grid gap-4 py-4">
+      <div>
+        <h1 className="text-secondary font-semibold text-xl text-center">
+          Fine Tune
+        </h1>
+      </div>
+      <div>
+        <form>
+          <input
+            type="file"
+            className="hidden"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+          />
+          <div className="flex flex-col gap-4 justify-center items-center rounded-md my-auto">
+            <button
+              type="button"
+              className="grid justify-self-center border border-secondary/70 border-dashed rounded-full p-8 w-fit h-fit"
+              onClick={handleButtonClick}
+            >
+              <Upload color="white" />
+            </button>
+
+            <div className="grid justify-center">
+              <h1 className="text-center text-sm text-secondary/70">
+                Enhance the performance of you AI model
+              </h1>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
--- components/dashboard/sub/train-ai/sub/UploadHandler.jsx
+++ components/dashboard/sub/train-ai/sub/UploadHandler.jsx
@@ -1,49 +1,26 @@
-import UploadFile from "./UploadFile";
-import { useState } from "react";
-import { BrainCog, SpellCheck } from "lucide-react";
+import UploadFineTune from "./UploadFineTune";
+import UploadValidation from "./UploadValidation";
 
 export default function UploadHandler() {
-  const [isActivated, setIsActivated] = useState(false);
-
   return (
     <div className="grid gap-4">
-      <div className="flex justify-between">
-        <div className="flex gap-4">
-          <button
-            className={`border border-secondary/70 px-2 py-1 rounded-md text-secondary bg-primary hover:text-primary hover:bg-secondary ${
-              !isActivated && "bg-secondary !text-primary"
-            }`}
-            onClick={() => setIsActivated(false)}
-          >
-            <li className="flex items-center gap-2">
-              <BrainCog size={18} /> Fine Tune
-            </li>
-          </button>
-          <button
-            className={`border border-secondary/70 px-2 py-1 rounded-md text-secondary bg-primary hover:text-primary hover:bg-secondary ${
-              isActivated && "bg-secondary !text-primary"
-            }`}
-            onClick={() => setIsActivated(true)}
-          >
-            <li className="flex items-center gap-2">
-              <SpellCheck size={18} /> Validation
-            </li>
-          </button>
+      <div className="flex gap-4 w-auto lg:w-[68vw]">
+        <div className="w-full h-full">
+          <div className="bg-primary w-full h-full flex flex-col items-center gap-8 border border-secondary/70 rounded-md ">
+            {/* Content */}
+          </div>
+          
         </div>
-      </div>
-      <div className="bg-primary flex flex-col items-center gap-8 border border-secondary/70 rounded-md w-auto lg:w-[68vw] min-h-[20.125rem]">
-        <UploadFile
-          title={
-            isActivated
-              ? "Upload validations data to enhance the AI"
-              : "Upload fine tuning data to train the AI"
-          }
-          isValidation={isActivated}
-        />
-      </div>
-      <div className="flex justify-between text-secondary text-sm">
-        <p>Supported Formats: JSONL</p>
-        <p>Maximum Size: 50MB</p>
+
+        <div className="grid gap-4 w-full h-full">
+          <div className="bg-primary w-full h-full flex flex-col items-center gap-8 border border-secondary/70 rounded-md ">
+            <UploadFineTune />
+          </div>
+          <div className="bg-primary w-full h-full flex flex-col items-center gap-8 border border-secondary/70 rounded-md ">
+            <UploadValidation />
+          </div>
+          
+        </div>
       </div>
     </div>
   );
