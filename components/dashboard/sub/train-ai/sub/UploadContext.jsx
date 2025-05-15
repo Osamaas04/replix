6811b1a932b490e99817891fe55ee62ac183c031
Osamaas04 <alasmarosama7@gmail.com>
2025-05-15 21:37:18 +0300

    refine 161

--- components/dashboard/sub/train-ai/sub/UploadContext.jsx
+++ components/dashboard/sub/train-ai/sub/UploadContext.jsx
@@ -18,6 +18,7 @@ export default function UploadContext({ contextFile }) {
   useEffect(() => {
     if (contextFile) {
       setLoading(true);
+      setSelectedFile(contextFile)
     }
   }, [contextFile]);
 
